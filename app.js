e105c91189635202600dedee2904a0f928f10c4f
Fan Jiang <fanjiang@thoughtworks.com>
2016-03-03 16:45:06 -0500

    generate static gameInfo page

--- app.js
+++ app.js
@@ -21,23 +21,54 @@ var gameList = {
     }
 };
 var app = express();
+function ensureDirectoryExistence(filePath) {
+  var dirname = path.dirname(filePath);
+  if (directoryExists(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+}
+
+function directoryExists(path) {
+  try {
+    return fs.statSync(path).isDirectory();
+  }
+  catch (err) {
+    return false;
+  }
+}
 app.use(express.static('public'));
 app.get('/games/info/:id/:from',  function(request, response){
     var from = request.params.from;
+    var id = request.params.id;
     gameInfo.load(
-        request.params.id,
+        id,
         function(err, res){
-            game = res.body.data;
-            var relPath = 'components/games/info.vue';
-            var gameView = new Vue({
-                template: String(fs.readFileSync(path.join(__dirname, relPath))),
-                data: {
-                    game: game,
-                    from: from
+            var staticRelPath = 'statics/games/info/'+id+'.html';
+            var staticPath = path.join(__dirname, staticRelPath);
+            fs.exists(staticPath,function(exists){
+                if(exists)
+                    response.sendFile(staticPath);
+                else{
+                    var componentRelPath = 'components/games/info.vue';
+                    var componentPath = path.join(__dirname, componentRelPath);
+                    game = res.body.data;
+                    var gameView = new Vue({
+                        template: String(fs.readFileSync(componentPath)),
+                        data: {
+                            game: game,
+                            from: from
+                        }
+                    });
+                    gameView.$on('vueServer.htmlReady', function(html) {
+                        response.send(html);
+                        fs.writeFile(staticPath, html, { flags: 'wx' }, function (err) {
+                            if (err) throw err;
+                            console.log("static file saved:"+staticRelPath);
+                        });
+                    });
                 }
-            });
-            gameView.$on('vueServer.htmlReady', function(html) {
-                response.send(html);
             });
         });
 });
